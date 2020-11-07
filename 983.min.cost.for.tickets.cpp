c5268ebe395d7341ea8312b141cea69859a99b4d
Touhid Alam <touhid@Touhids-MacBook-Pro.local>
2020-11-08 03:23:04 +0600

    update

--- 983.min.cost.for.tickets.cpp
+++ 983.min.cost.for.tickets.cpp
@@ -1,41 +1,28 @@
 /**
- * @file 983.min.cost.for.tickets.cpp
+ * @file 983.min.cost.for.tickets
  * @author Touhid Alam <taz.touhid@gmail.com>
  *
- * @date Sat July 18 2020
+ * @date Tuesday October 20 2020
  *
- * @brief
+ * @brief 
  */
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int INF = 1e9 + 7;
-
 class Solution {
  public:
-  int mincostTickets(vector<int>& d, vector<int>& c) {
-    unordered_set<int> s(d.begin(), d.end());
+  int mincostTickets(vector<int>& days, vector<int>& c) {
+    unordered_set<int> d(days.begin(), days.end());
+    int dp[30] = {};
 
-    int n = d[d.size() - 1];
-    int dp[n + 1];
-
-    dp[0] = 0;
-    for(int i = 1; i < n + 1; ++i) {
-        dp[i] = INF;
-    }
-    
-    for(int i = 1; i <= n; ++i) {
-      if(!s.count(i)) dp[i] = dp[i - 1];
-      else dp[i] = min({
-        dp[i],
-        dp[max(i - 01, 0)] + c[0],
-        dp[max(i - 07, 0)] + c[1],
-        dp[max(i - 30, 0)] + c[2]
-      });
+    for (int i = days[0]; i <= days[days.size() - 1]; ++i) {
+      if (!d.count(i)) dp[i % 30] = dp[(i - 1) % 30];
+      else
+        dp[i % 30] = min(dp[max(0, i - 1) % 30] + c[0], min(dp[max(0, i - 7) % 30] + c[1], dp[max(0, i - 30) % 30] + c[2]));
     }
 
-    return dp[n];
+    return dp[days[days.size() - 1] % 30];
   }
 };
